064c6f364147ab947c30620d5c9a291a2bab2f7d
Artem <79601745+Sovenok-Hacker@users.noreply.github.com>
2022-04-23 21:08:41 +0300

    Add files via upload

---
+++ md5.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+import hashlib
+print(hashlib.md5(input().encode()).hexdigest())
---
+++ sha256.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+import hashlib
+print(hashlib.sha256(input().encode()).hexdigest())
